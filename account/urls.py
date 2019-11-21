f8f9106a0b2b7fe359635052ecc693f0b0dfc028
MINJUN <good_day1234@naver.com>
2019-11-21 13:06:11 +0000

    add more detail feature on account app

--- account/urls.py
+++ account/urls.py
@@ -3,8 +3,10 @@ from . import views
 
 app_name= "account"
 urlpatterns = [
-    path('', views.signin, name="signin"),
+    path('signin/', views.signin, name="signin"),
     path('signup/', views.signup, name="signup"),
     path('signout/', views.signout, name="signout"),
-    path('loginchk/', views.loginchk, name="loginchk"),
+    path('', views.loginchk, name="loginchk"),
+    path('edituser/', views.edituser, name="edituser"),
+    path('loginonly/', views.loginonly, name="loginonly"),
 ]
--- account/views.py
+++ account/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render, redirect
 # 로그인 구현을 위한 auth
 from django.contrib.auth.models import User
 from django.contrib import auth
-
+from django.contrib.auth.decorators import login_required
 
 def signup(request):
+    if request.user.is_authenticated:
+        # 로그인이 안되어있는 상태에서만 진입 가능
+        return redirect('account:loginchk')
     if request.method == "POST":
         if request.POST["password1"] == request.POST["password2"]:
             user = User.objects.create_user(username=request.POST["username"],password=request.POST["password1"])
@@ -16,24 +19,55 @@ def signup(request):
 
 
 def signin(request):
+    if request.user.is_authenticated:
+        return redirect('account:loginchk')
+    
     if request.method == "POST":
         username=request.POST["username"]
         password=request.POST["password"]
         user = auth.authenticate(request, username=username, password=password)
         if user is not None:
             auth.login(request, user)
-            # request.session['username'] = username
+            
             return redirect('account:loginchk')
         else:
             return render(request, 'signin.html', {'error': '어디 내앞에서 감히 거짓말인가!'})
     return render(request, 'signin.html')
 
-
+@login_required(login_url='account:signin')
 def signout(request):
     auth.logout(request)
     return redirect('account:loginchk')
+
 
 def loginchk(request):
     
     return render(request, 'chk.html')
 
+
+@login_required(login_url='account:signin')
+def edituser(request):
+    if request.method == 'POST':
+        username = request.POST["username"]
+        password = request.POST["password1"]
+        user = auth.authenticate(username=username, password=request.POST["old_password"])
+        if user is not None:
+            if password == request.POST["password2"]:
+                edit_user = User.objects.get(username=username)
+                edit_user.set_password(password)
+                edit_user.save()
+                
+                auth.login(request, edit_user)
+                return redirect('account:loginchk')
+            else:
+                return render(request, 'edituser.html', {'error': '비밀번호가 일치하지 않습니다.'})
+        else:
+            return render(request, 'edituser.html', {'error': '사용자가 확인되지 않습니다.'})
+        
+        
+    return render(request, 'edituser.html')
+
+
+@login_required(login_url='account:signin')
+def loginonly(request):
+    return render(request, 'login_only.html')
